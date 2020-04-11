b27d15ebd77a5a19d609223615701b1298de18cb
Orlando Frade <fradeneto@gmail.com>
2020-04-10 23:12:31 -0300

    feat: added get ip

--- index.js
+++ index.js
@@ -2,14 +2,22 @@ require('dotenv/config');
 const express = require('express');
 const app = express();
 const port = process.env.PORT;
-
+app.use(express.json())
 app.use('/', function (req, res) {
-  const { query, body } = req;
+  const { query, body, headers } = req;
+  const { remoteAddress } = req.connection;
+  const socketRemoteAddress = req.socket.remoteAddress ? req.socket.remoteAddress : '';
+  const connSckRemoteAddress = req.connection.socket ? req.connection.socket.remoteAddress : '';
+
   res.status(200).json({
     method: req.method,
     uri: req.url,
     query,
     body,
+    headers,
+    remoteAddress,
+    socketRemoteAddress,
+    connSckRemoteAddress,
   })
 });
 
